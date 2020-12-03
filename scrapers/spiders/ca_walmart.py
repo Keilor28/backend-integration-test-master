b323c4cc1f0a694acca958f40f9ffeb45ad9dc2e
Keilor Rivera <kfriverar@est.utn.ac.cr>
2020-12-02 23:42:05 -0600

    Case 1, Just missing branch

--- scrapers/spiders/ca_walmart.py
+++ scrapers/spiders/ca_walmart.py
@@ -1,12 +1,23 @@
 import scrapy
-
+import json
+from scrapy import Request
 from scrapers.items import ProductItem
+from scrapy.exceptions import CloseSpider
 
 
 class CaWalmartSpider(scrapy.Spider):
     name = "ca_walmart"
-    allowed_domains = ["walmart.ca"]
+    allowed_domains = ["www.walmart.ca"]
     start_urls = ["https://www.walmart.ca/en/grocery/fruits-vegetables/fruits/N-3852"]
 
-    def parse(self, response):
-        pass
+
+def parse(self, response):
+        list_links_fruits = response.css('a.product-link::attr("href")').getall()
+        for product in list_links_fruits:
+            url_product = self.base_walmart_url + product
+            yield Request(url=url_product, callback=self.product_detail, cb_kwargs={'url': url_product})
+
+        next_page = response.css('#loadmore::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+        
